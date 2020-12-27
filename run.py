a966686eda8723e5e59d863112e3e3280c6cb851
Danny Dasilva <yahchayildasilva@gmail.com>
2020-12-26 21:55:34 -0300

    Update run.py

--- run.py
+++ run.py
@@ -7,3 +7,41 @@ B = 63
 Import math
 
 math.sqrt(A ** 2 + B ** 2)
+
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+import string
+
+# points a, b and, c
+a, b, c, d = (0, 1, 0), (1, 0, 1), (0, -1, 2), (-1, 0, 3)
+
+# matrix with row vectors of points
+A = np.array([a, b, c, d])
+
+# 3x3 Identity transformation matrix
+I = np.eye(3)
+
+
+color_lut = 'rgbc'
+fig = plt.figure()
+ax = plt.gca()
+xs = []
+ys = []
+for row in A:
+    output_row = I @ row
+    x, y, i = output_row
+    xs.append(x)
+    ys.append(y)
+    i = int(i) # convert float to int for indexing
+    c = color_lut[i]
+    plt.scatter(x, y, color=c)
+    plt.text(x + 0.15, y, f"{string.ascii_letters[i]}")
+xs.append(xs[0])
+ys.append(ys[0])
+plt.plot(xs, ys, color="gray", linestyle='dotted')
+ax.set_xticks(np.arange(-2.5, 3, 0.5))
+ax.set_yticks(np.arange(-2.5, 3, 0.5))
+plt.grid()
+plt.show()
